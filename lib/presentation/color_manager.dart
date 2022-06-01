4ce987f0f591a4bb81d0360904b8f3443a8b4d4e
Last-dreamer <asim0000.ak@gmail.com>
2022-06-02 01:24:21 +0500

    adding font and color manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,24 @@
+
+
+import 'dart:ui';
+
+
+class ColorManager {
+
+  static Color primary =  HexColor.fromHex("#Ed9728");
+  static Color darkGrey =  HexColor.fromHex("#525252");
+  static Color grey = HexColor.fromHex("#737477");
+  static Color lightGrey = HexColor.fromHex("#9E9E9E");
+}
+
+extension HexColor on Color{
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll("#", "");
+    if (hexColorString.length == 6) {
+      hexColorString = "FF" + hexColorString;
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
+
+
